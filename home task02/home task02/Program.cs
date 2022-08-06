0f62edab48bd22b48aade09e4036b33085affcc4
Jalyastyle <jalaag@code.edu.az>
2022-08-06 23:16:04 +0400

    second task

---
+++ home task02/home task02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class task02
+{
+	public static int SpaceCount(string str)
+	{
+		int spcctr = 0;
+		string str1;
+		for (int i = 0; i < str.Length; i++)
+		{
+			str1 = str.Substring(i, 1);
+			if (str1 == " ")
+				spcctr++;
+		}
+		return spcctr;
+	}
+	public static void Main()
+	{
+		string str2;
+		Console.Write("Function to count number of spaces in a string :");
+		Console.Write("Please input a string : ");
+		str2 = Console.ReadLine();
+		Console.WriteLine("" + str2 + "" + " contains {0} spaces", SpaceCount(str2));
+	}
+}
+
